e08e569f111a5ddaa6fa6d9cb27acf7b2235f03a
chaerish <ccyy8432@naver.com>
2025-05-26 16:08:53 +0900

    init: initial project setup

---
+++ src/main/java/com/skala/nav7/Nav7Application.java
@@ -0,0 +1,13 @@
+package com.skala.nav7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nav7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nav7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/skala/nav7/Nav7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skala.nav7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nav7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
