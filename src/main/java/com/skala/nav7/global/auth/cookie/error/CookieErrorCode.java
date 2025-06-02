131c1fc9ab6ecc87a72812e05cbc88a4f92a7a22
chaerish <ccyy8432@naver.com>
2025-06-02 09:21:50 +0900

    feat(Cookie): 쿠키 관련 에러 코드 설정

---
+++ src/main/java/com/skala/nav7/global/auth/cookie/error/CookieErrorCode.java
@@ -0,0 +1,26 @@
+package com.skala.nav7.global.auth.cookie.error;
+
+import com.skala.nav7.global.apiPayload.code.base.BaseErrorCode;
+import com.skala.nav7.global.apiPayload.dto.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum CookieErrorCode implements BaseErrorCode {
+    REFRESH_TOKEN_MISSING(HttpStatus.BAD_REQUEST, "TOKEN4005", "쿠키에 리프레쉬 토큰이 존재하지 않습니다.");
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ErrorReasonDTO getReasonHttpStatus() {
+        return ErrorReasonDTO.builder()
+                .httpStatus(this.status)
+                .isSuccess(false)
+                .code(this.code)
+                .message(this.message)
+                .build();
+    }
+}
