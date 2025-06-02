e26ae6eefa43be4a6ba5d2f32eda03fce5889350
chaerish <ccyy8432@naver.com>
2025-06-02 15:20:32 +0900

    feat(CorsConfig): 다른 Origin 요청 허용 Cors 설정

---
+++ src/main/java/com/skala/nav7/global/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.skala.nav7.global.config;
+
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(
+                List.of("https://localhost:3000", "https://sk-nav7.skala25a.project.skala-ai.com"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
