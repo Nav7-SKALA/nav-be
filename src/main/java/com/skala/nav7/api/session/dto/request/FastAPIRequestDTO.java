928ef564138a940e0953950e3ca8b79d374d457f
chaerish <ccyy8432@naver.com>
2025-06-05 13:00:25 +0900

    refactor(DTO): fastAPI 요청 시 Request Body DTO 구조 동적으로 변경

--- src/main/java/com/skala/nav7/api/session/dto/request/FastAPIRequestDTO.java
+++ src/main/java/com/skala/nav7/api/session/dto/request/FastAPIRequestDTO.java
@@ -8,11 +8,14 @@ public record FastAPIRequestDTO(
             @Schema(description = "사용자 ID", example = "1")
             String user_id,
 
+            @Schema(description = "세션 ID", example = "ad32048a-3b55-4ddb-bee5-b9ffe1c06cee")
+            String session_id,
+
             @Schema(description = "사용자의 질문", example = "어떤 경로로 개발자가 될 수 있을까요?")
             String user_query
     ) {
-        public static CareerPathRequestDTO of(Long profileId, String question) {
-            return new CareerPathRequestDTO(String.valueOf(profileId), question);
+        public static CareerPathRequestDTO of(Long profileId, String sessionId, String question) {
+            return new CareerPathRequestDTO(String.valueOf(profileId), sessionId, question);
         }
     }
 }
--- src/main/java/com/skala/nav7/api/session/dto/response/FastAPIResponseDTO.java
+++ src/main/java/com/skala/nav7/api/session/dto/response/FastAPIResponseDTO.java
@@ -1,36 +1,25 @@
 package com.skala.nav7.api.session.dto.response;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import java.util.HashMap;
-import lombok.Builder;
-
+import com.fasterxml.jackson.databind.JsonNode;
 
 public record FastAPIResponseDTO(
+        Content content
 ) {
-    public record CareerResponseDTO(
-            @Schema(description = "응답 데이터")
-            Content content
+    public record Content(
+            boolean success,
+            Result result
     ) {
-        @Builder
-        @Schema(description = "FastAPI 최상위 content 객체")
-        public record Content(
-                @Schema(description = "성공 여부", example = "true")
-                boolean success,
+    }
 
-                @Schema(description = "실제 결과")
-                CareerDTO result
-        ) {
-        }
+    public record Result(
+            String agent,
+            JsonNode text
+    ) {
+    }
 
-        @Builder
-        @Schema(description = "Career 관련 응답 DTO")
-        public record CareerDTO(
-                @Schema(description = "Agent 종류", example = "CareerSummary")
-                String agent,
-
-                @Schema(description = "AI 응답 맵", example = "{\"summary\":\"요약\", \"goal\":\"목표\"}")
-                HashMap<String, String> map
-        ) {
-        }
+    public record RoleModelDTO(
+            Long profileId,
+            double similarity_score
+    ) {
     }
 }
