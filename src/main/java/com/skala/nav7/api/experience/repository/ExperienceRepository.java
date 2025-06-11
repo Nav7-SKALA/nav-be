289b874c9b32c1073cba8ee484155ceb01a33da3
chaerish <ccyy8432@naver.com>
2025-06-11 10:54:30 +0900

    feat(Experience): Experience 관련 엔티티 생성 및 Repository 생성

--- src/main/java/com/skala/nav7/api/experience/repository/ExperienceRepository.java
+++ src/main/java/com/skala/nav7/api/experience/repository/ExperienceRepository.java
@@ -2,9 +2,13 @@ package com.skala.nav7.api.experience.repository;
 
 
 import com.skala.nav7.api.experience.entity.Experience;
+import com.skala.nav7.api.profile.entity.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExperienceRepository extends JpaRepository<Experience, Long> {
+    Page<Experience> findAllByProfile(Profile profile, Pageable pageable);
 }
